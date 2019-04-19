ba6f730cd875f83c2d50ca476fb9edd29f5d4d14
Rachel Loziuk <rachel@reviewpush.com>
2019-04-19 13:54:05 -0700

    create a product controller for the API.

---
+++ app/Http/Controllers/Api/ProductsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Product;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Products::all();
+
+        return $products->json();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+
+        return $product->json();
+    }
+
+}
