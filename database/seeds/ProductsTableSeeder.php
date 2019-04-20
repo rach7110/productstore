b8f14f75c3650dac0f9cb6e07b566c5600bb379c
Rachel Loziuk <rachel@reviewpush.com>
2019-04-20 13:36:13 -0700

    add a table seeder.

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Product;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->truncate();
+
+        factory(Product::class, 5)->create();
+    }
+}
