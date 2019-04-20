649e9846130351e3ea652f035632c033cb5620e2
Rachel Loziuk <rachel@reviewpush.com>
2019-04-20 13:20:08 -0700

    WIP: sync our database with sales channel product quanitites.

--- app/Http/Controllers/Api/ProductsController.php
+++ app/Http/Controllers/Api/ProductsController.php
@@ -5,9 +5,26 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Product;
+use App\Services\ShopifyService;
+use App\Services\VendService;
 
 class ProductsController extends Controller
 {
+    protected $vendSrv;
+    protected $shopifySvc;
+
+
+    /**
+     * Create a new instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->vendSrv = new VendService;
+        $this->shopifySvc = new ShopifyService;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -33,4 +50,29 @@ class ProductsController extends Controller
         return response()->json($product);
     }
 
+    /**
+     * Sync our database with Sales Channels.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sync()
+    {
+        $vendProducts = collect(json_decode($this->vendSrv->call(), true));
+        $shopifyProducts = collect(json_decode($this->shopifySvc->call(), true));
+
+        $products = $vendProducts
+            ->merge($shopifyProducts)
+            ->groupBy('sku');
+
+        foreach($products as $product) {
+            // Compile quantities from multiple sales channels.
+            if($product->count() > 1) {
+
+            } else {
+                $product = $product->first();
+                $stitchProduct = Product::firstOrCreate(['sku' => $product['sku'] ]);
+            }
+        }
+
+    }
 }
