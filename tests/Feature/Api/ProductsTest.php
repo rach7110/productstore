9e690670094f774bf77a36092a446722c2eef2dc
Rachel Loziuk <rachel@reviewpush.com>
2019-04-20 13:42:12 -0700

    update test.

--- tests/Feature/Api/ProductsTest.php
+++ tests/Feature/Api/ProductsTest.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ProductsTest extends TestCase
 {
-    use RefreshDatabase;
+    // use RefreshDatabase;
     /**
      * A basic functional test example.
      *
@@ -17,16 +17,41 @@ class ProductsTest extends TestCase
     public function testReturnsProductsInJsonFormat()
     {
         factory(Product::class, 4)->create();
-        $product = Product::findOrFail(1);
+        $product1 = Product::findOrFail(1);
+        $product2 = Product::findOrFail(2);
 
         $response = $this->json('GET', 'api/products');
         $response
             ->assertStatus(200)
-            ->assertJson([[
+            ->assertJson([
+                [
+                'name'      => $product1->name,
+                'sku'       => $product1->sku,
+                'quantity'  => $product1->quantity,
+                'price'     => $product1->price
+                ],
+                [
+                'name'      => $product2->name,
+                'sku'       => $product2->sku,
+                'quantity'  => $product2->quantity,
+                'price'     => $product2->price
+                ]
+            ]);
+    }
+
+    public function testReturnsProductInJsonFormat()
+    {
+        factory(Product::class)->create();
+        $product = Product::findOrFail(1);
+
+        $response = $this->json('GET', 'api/products/1');
+        $response
+            ->assertStatus(200)
+            ->assertJson([
                 'name'      => $product->name,
                 'sku'       => $product->sku,
                 'quantity'  => $product->quantity,
                 'price'     => $product->price
-            ]]);
+            ]);
     }
 }
